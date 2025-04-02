3338d5751605f2ab2b2f5998ac686a74d9fc180f
Wasiu Idowu <hoxtygen@live.com>
2025-04-02 23:23:52 +0100

    Add service layer
    This service layer contains the business logic.

---
+++ src/main/java/com/codeplanks/taskManager/service/TaskService.java
@@ -0,0 +1,98 @@
+package com.codeplanks.taskManager.service;
+
+import com.codeplanks.taskManager.model.task.AllTaskResponse;
+import com.codeplanks.taskManager.model.task.TaskResponseDTO;
+import com.codeplanks.taskManager.repository.TaskRepository;
+import com.codeplanks.taskManager.utils.LogUtils;
+import com.codeplanks.taskManager.utils.QueryUtils;
+import io.vertx.core.Future;
+import io.vertx.sqlclient.Pool;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TaskService {
+
+  private static final Logger logger = LoggerFactory.getLogger(
+    TaskService.class
+  );
+
+  private final Pool dbClient;
+  private final TaskRepository taskRepository;
+
+  public TaskService(Pool dbClientPool, TaskRepository taskRepository) {
+    this.dbClient = dbClientPool;
+    this.taskRepository = taskRepository;
+  }
+
+  public Future<AllTaskResponse> getAllTasks(String page, String limit) {
+    // obtain a connection from the pool
+    return dbClient
+      .withTransaction(connection -> {
+        final int pageNumber = QueryUtils.getPage(page);
+        final int limitNumber = QueryUtils.getLimit(limit);
+        final int offset = QueryUtils.getOffset(pageNumber, limitNumber);
+        return taskRepository
+          .count(connection)
+          .flatMap(total ->
+            taskRepository
+              .getAllTasks(connection, limitNumber, offset)
+              .map(result -> {
+                final List<TaskResponseDTO> tasks = result
+                  .stream()
+                  .map(TaskResponseDTO::new)
+                  .collect(Collectors.toList());
+
+                return new AllTaskResponse(
+                  total,
+                  limitNumber,
+                  pageNumber,
+                  tasks
+                );
+              })
+          );
+      })
+      .onSuccess(success ->
+        logger.info(
+          LogUtils.REGULAR_CALL_SUCCESS_MESSAGE.buildMessage(
+            "Get all tasks",
+            success.getTasks()
+          )
+        )
+      )
+      .onFailure(failure ->
+        logger.error(
+          LogUtils.REGULAR_CALL_ERROR_MESSAGE.buildMessage(
+            "Get all tasks",
+            failure.getMessage()
+          )
+        )
+      );
+  }
+
+  public Future<TaskResponseDTO> getTaskById(int taskId) {
+    return dbClient
+      .withTransaction(connection -> {
+        return taskRepository
+          .getByTaskId(connection, taskId)
+          .map(TaskResponseDTO::new);
+      })
+      .onSuccess(success ->
+        logger.info(
+          LogUtils.REGULAR_CALL_SUCCESS_MESSAGE.buildMessage(
+            "Get one task",
+            success
+          )
+        )
+      )
+      .onFailure(throwable ->
+        logger.error(
+          LogUtils.REGULAR_CALL_ERROR_MESSAGE.buildMessage(
+            "Get one task",
+            throwable.getMessage()
+          )
+        )
+      );
+  }
+}
